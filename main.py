04121372dcfa76a5c0c502a391c0a7b6a98bbdcb
Phyu Lwin <76246754+phyulwin@users.noreply.github.com>
2022-06-18 22:58:54 +0000

    Configured Login

--- main.py
+++ main.py
@@ -1,19 +1,56 @@
-#move code to main after testing and linking with html
-import json
+"""Python Flask WebApp Auth0 integration example
+"""
 import os
 import re
+import json
+from os import environ as env
+from urllib.parse import quote_plus, urlencode
 
-from flask import Flask, redirect, render_template, session, url_for, request
+from authlib.integrations.flask_client import OAuth
+from dotenv import find_dotenv, load_dotenv
+from flask import Flask, redirect, render_template, session, url_for
+
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
 
 app = Flask(__name__)
-#go to homepage (index.hmtl)
+app.secret_key = os.environ['APP_SECRET_KEY']
+
+oauth = OAuth(app)
+
+oauth.register(
+    "auth0",
+    client_id=os.environ['AUTH0_CLIENT_ID'],
+    client_secret=os.environ['AUTH0_CLIENT_SECRET'],
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{os.environ["AUTH0_DOMAIN"]}/.well-known/openid-configuration',
+)
+
+# Controllers API
 @app.route("/")
 def home():
-    return render_template("index.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
+    return render_template(
+        "index.html",
+        session=session.get("user"),
+        #pretty=json.dumps(session.get("user"), indent=4),
+    )
 
-#code to run server
+
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token
+    return redirect("/")
+
+
+@app.route("/login")
+def login():
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=env.get("PORT", 3000))
-
-#if the above port is occupied, use the one below
-#app.run(host='0.0.0.0', port=81)
